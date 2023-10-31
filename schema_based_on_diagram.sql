d254871976b61b192455fe09d35537910c62cec3
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-10-31 13:54:55 +0430

    create tables with its relationship

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE clinic;
+CREATE TABLE  patients (id SERIAL PRIMARY KEY, name VARCHAR(50), date_of_birth DATE);
+CREATE TABLE  medical_histories (id SERIAL PRIMARY KEY, admitted_at  TIMESTAMP, patient_id INT, status VARCHAR(50));
+ALTER TABLE medical_histories ADD CONSTRAINT fk_patient FOREIGN KEY (patient_id) REFERENCES patients(id);
+CREATE TABLE invoices (id SERIAL PRIMARY KEY, total_amount DECIMAL, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INT);
+ALTER TABLE invoices ADD CONSTRAINT fk_medical_history_id FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id);
+CREATE TABLE invoices_items (id SERIAL PRIMARY KEY, unit_price DECIMAL, quantity INT, total_price DECIMAL, invoice_id INT,treatment_id INT);
+ALTER TABLE invoices_items ADD CONSTRAINT fk_invoice_id FOREIGN KEY (invoice_id) REFERENCES invoices(id);
+CREATE TABLE treatments (id SERIAL PRIMARY KEY, type VARCHAR(50), name VARCHAR(50));
+ALTER TABLE invoices_items ADD CONSTRAINT fk_treatment_id FOREIGN KEY (treatment_id) REFERENCES treatments(id);
+CREATE TABLE treatments (id SERIAL PRIMARY KEY, type VARCHAR(50), name VARCHAR(50));
+ALTER TABLE invoices_items ADD CONSTRAINT fk_treatment_id FOREIGN KEY (treatment_id) REFERENCES treatments(id);
+CREATE TABLE medical_histories_treatment (medical_history_id INT REFERENCES medical_histories(id), treatment_id INT REFERENCES treatments(id));
